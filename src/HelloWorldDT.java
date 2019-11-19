bc018c1436ecec6bb04b12f75bdfdea2ab3a64d5
dtriantaphyllis <57912121+dtriantaphyllis@users.noreply.github.com>
2019-11-19 14:26:35 -0600

    Delete

--- src/HelloWorldDT.java
+++ src/HelloWorldDT.java
@@ -1,12 +1,5 @@
 
 public class HelloWorldDT
 {
-	public static void main(String args[])
-	{
-		System.out.println("hello world");
-		for(int i = 0; i <= 10; i++)
-		{
-			System.out.println(i);
-		}
-	}
+//lmao i deleted everything
 }
